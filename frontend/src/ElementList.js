97a4ba21f5955bcc1dd548fdefdc053f44808b65
garion.musetta <icedgarion@gmail.com>
2022-08-02 14:21:18 +0200

    Conditional category

--- frontend/src/ElementList.js
+++ frontend/src/ElementList.js
@@ -111,7 +111,7 @@ class ElementList extends Component {
             return <tr key={element.id}>
                 <td><div contentEditable="true" onBlur={ e => this.editText(element.id, "nome", e.currentTarget.textContent) }>{element.nome}</div></td>
                 <td><div contentEditable="true" onBlur={ e => this.editText(element.id, "dove", e.currentTarget.textContent) }>{element.dove}</div></td>
-                <td><div contentEditable="true" onBlur={ e => this.editText(element.id, "categoria", e.currentTarget.textContent) }>{element.categoria}</div></td>
+                <td hidden={this.state.activeTab === 'cibo'} ><div contentEditable="true" onBlur={ e => this.editText(element.id, "categoria", e.currentTarget.textContent) }>{element.categoria}</div></td>
                 <td><div contentEditable="true" onBlur={ e => this.editText(element.id, "note", e.currentTarget.textContent) }>{element.note}</div></td>
                 <td><div style={{display:"flex"}}><Button size="lg" style={{textAlign:"center",display:"block",margin:"auto", flex:"auto"}} color={element.disponibile === true ? "success" : "danger"} onClick={ e => this.editText(element.id, "disponibile", !element.disponibile)}></Button></div></td>
                 <td><Button size="sm" color="dark" style={{textAlign:"center",display:"block",margin:"auto", flex:"auto"}} onClick={() => this.remove(element.id)}>-</Button></td>
@@ -146,7 +146,7 @@ class ElementList extends Component {
                         <tr>
                             <th width="19%"><h5>Nome</h5></th>
                             <th width="19%"><h5>Dove</h5></th>
-                            <th width="10%"><h5>Categoria</h5></th>
+                            <th hidden={this.state.activeTab === 'cibo'} width="10%"><h5>Tipo</h5></th>
                             <th width="19%"><h5>Note</h5></th>
                             <th width="19%"><h5>Manca</h5></th>
                             <th width="9%"></th>
